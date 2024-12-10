460d8fa6d4b93c98438621c29259b5a0a7af902a
RicardooGM <160785633+RicardooGM@users.noreply.github.com>
2024-12-10 18:27:01 +0000

    arquivos

--- Home.py
+++ Home.py
@@ -1,5 +1,7 @@
 import streamlit as st
 #criar um arquivo venv para as bibliotecas, ai quando entrar denovo, é só ativar o venv e rodar o codigo Home.py
+st.set_page_config(layout="wide")
+
 st.header("Economics", divider="rainbow")
 
 col1, col2 = st.columns(2,gap="large")
@@ -10,3 +12,24 @@ with col1:
 with col2:
     st.subheader("Taxa de juros")
     st.container(height = 200)
+
+
+
+st.write("### Metric")
+
+col1, col2, col3 = st.columns(3, gap = "large")
+
+with col1:
+    with st.container(height = 200):
+        st.subheader("eua")
+        st.metric("Temperature", "70 F", "1.2 F")
+
+with col2:
+    with st.container(height = 200):
+        st.subheader("juros")
+        st.metric("Temperature", "70 F", "1.2 F")
+
+with col3:
+    with st.container(height = 200):
+        st.subheader("moeda")
+        st.metric("Temperature", "70 F", "1.2 F")
